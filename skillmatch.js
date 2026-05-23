33c20ea01deccfb5ced8dc099fcb5555f9ba0c07
ALAIANE MAIARA FERREIRA DOS SANTOS <santoslala880@gmail.com>
2026-05-23 16:34:45 -0300

    feat: candidato e vagas

--- skillmatch.js
+++ skillmatch.js
@@ -33,4 +33,58 @@ class VagaFrontEnd extends Vaga {
     return `Nível da vaga: ${this.nivel}`;
   }
 }
+ // Dados do candidato
  
+const candidato = {
+  nome: "Julia",
+  area: "Front-End",
+  habilidades: [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "Git",
+    "GitHub",
+    "Lógica de Programação",
+  ],
+  experienciaMeses: 3
+};
+ 
+// vagas  
+const vagas = [
+  new VagaFrontEnd(
+    1,
+    "StarTech",
+    "Estágio de Desenvolvedor Front-End Junior",
+    ["JavaScript", "GitHub", "Lógica de Programação", "Arrays"],
+    1500,
+    "Remoto",
+    "Júnior"
+  ),
+  new VagaFrontEnd(
+    2,
+    "SkyCode",
+    "Desenvolvedor Front-End Júnior",
+    ["JavaScript", "Kanban", "GitHub", "Git", "Objetos"],
+    3100,
+    "Presencial",
+    "Júnior"
+  ),
+  new VagaFrontEnd(
+    3,
+    "Quantumn Solutions",
+    "Programador JavaScript Junior",
+    ["JavaScript", "HTML", "Git"],
+    2900,
+    "Híbrido",
+    "Júnior"
+  ),
+  new VagaFrontEnd(
+    4,
+    "Solutions Web",
+    "Estágio Programador JavaScript",
+    ["JavaScript", "HTML", "GitHub", "Java"],
+    1900,
+    "Híbrido",
+    "Estágio"
+  )
+];
