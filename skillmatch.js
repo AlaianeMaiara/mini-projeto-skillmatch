12e2bec1f9fc25cb3d361bf599846351b18afe98
ALAIANE MAIARA FERREIRA DOS SANTOS <santoslala880@gmail.com>
2026-05-23 16:28:49 -0300

    feat: estrutura inicial de vagas com classe e herança

---
+++ skillmatch.js
@@ -0,0 +1,36 @@
+class Vaga {
+  constructor(id, empresa, cargo, requisitos, salario, modalidade) {
+    this.id = id;
+    this.empresa = empresa;
+    this.cargo = cargo;
+    this.requisitos = requisitos;
+    this.salario = salario;
+    this.modalidade = modalidade;
+  }
+ 
+  exibirResumo() {
+    return `${this.cargo} na empresa ${this.empresa}`;
+  }
+}
+ 
+// Herança das vagas 
+class VagaFrontEnd extends Vaga {
+  constructor(
+    id,
+    empresa,
+    cargo,
+    requisitos,
+    salario,
+    modalidade,
+    nivel
+  ) {
+    super(id, empresa, cargo, requisitos, salario, modalidade);
+ 
+    this.nivel = nivel;
+  }
+ 
+  exibirNivel() {
+    return `Nível da vaga: ${this.nivel}`;
+  }
+}
+ 
